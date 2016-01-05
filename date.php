8fbf4b24b3a2740b292ffd8b9292cabfbcf00e4d
kaffenkj <kaffenkj@gmail.com>
2016-01-05 11:55:27 -0500

    Create date.php

---
+++ date.php
@@ -0,0 +1,16 @@
+<?php
+header("Content-Type: image/png");
+$im = @imagecreate(100, 20)
+    or die("Cannot Initialize new GD image stream");
+$background_color = imagecolorallocate($im, 255, 255, 255);
+$text_color = imagecolorallocate($im, 0, 0, 0);
+$barcode = $_GET['barcode'];
+if ($barcode===32826012040648) {
+	$date = date('n/d/y',strtotime("+3 days"));
+} else {
+	$date = date('n/d/y',strtotime("+1 week"));
+}
+imagestring($im, 6, 5, 5, $date , $text_color);
+imagepng($im);
+imagedestroy($im);
+?>
